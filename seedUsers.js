9195169f1e29fd4f213d7e9dca4053566e9f5fa4
kasubagumummij2024-byte <kasubagumummij2024@gmail.com>
2025-11-21 08:38:14 +0700

    Fix: Ganti area

--- seedUsers.js
+++ seedUsers.js
@@ -27,14 +27,14 @@ const usersData = [
   { nip: "20191101349", nama: "Luthfiyah Nuur Janah, S.Kom", area: "Madrasah Ibtidaiyah", jabatan: "Staf TU", role: "STAFF_TU" },
   { nip: "20181001312", nama: "Syahril Sidik, S.Ag", area: "Madrasah Tsanawiyah", jabatan: "Kamad", role: "KAMAD" },
   { nip: "20181101163", nama: "Nuryanti, SE, M.Pd", area: "Raudhatul Athfal", jabatan: "Kamad", role: "KAMAD" },
-  { nip: "20211101375", nama: "H. Mochammad Taufiqurrahman, SQ, MA.", area: "Satuan Kerja", jabatan: "Direktur", role: "DIRECTOR" },
-  { nip: "20070402046", nama: "Zatiah Lesyani, M.Pd", area: "Satuan Kerja", jabatan: "Kabag KHK", role: "KABAG" },
-  { nip: "20070402048", nama: "Dr. Muhamad Alwi, M.Pd", area: "Satuan Kerja", jabatan: "Kabag PM", role: "KABAG" },
-  { nip: "20110501100", nama: "Mohamat Ibrahim, S.Pd", area: "Satuan Kerja", jabatan: "Kabag TU & Umum", role: "KABAG" },
+  { nip: "20211101375", nama: "H. Mochammad Taufiqurrahman, SQ, MA.", area: "Direktur", jabatan: "Direktur", role: "DIRECTOR" },
+  { nip: "20070402046", nama: "Zatiah Lesyani, M.Pd", area: "Keuangan, Humas, dan Kepegawaian", jabatan: "Kabag KHK", role: "KABAG" },
+  { nip: "20070402048", nama: "Dr. Muhamad Alwi, M.Pd", area: "Penjamin Mutu", jabatan: "Kabag PM", role: "KABAG" },
+  { nip: "20110501100", nama: "Mohamat Ibrahim, S.Pd", area: "Tata Usaha dan Umum", jabatan: "Kabag TU & Umum", role: "KABAG" },
   { nip: "20150701214", nama: "Sarif Hidayat, S.Pd., M.Si", area: "Madrasah Aliyah", jabatan: "Staf TU", role: "STAFF_TU" },
   { nip: "20090401065", nama: "Mulana, A.Md.Kom", area: "Madrasah Tsanawiyah", jabatan: "Staf TU", role: "STAFF_TU" },
   { nip: "20010920011", nama: "Siti Sundari", area: "Raudhatul Athfal", jabatan: "Staf TU", role: "STAFF_TU" },
-  { nip: "20250201448", nama: "Hilman Alfarobi, S.A.P", area: "Satuan Kerja", jabatan: "Staf Administrasi Umum", role: "ADMIN_UMUM" }
+  { nip: "20250201448", nama: "Hilman Alfarobi, S.A.P", area: "Tata Usaha dan Umum", jabatan: "Staf Administrasi Umum", role: "ADMIN_UMUM" }
 ];
 
 async function seedUsers() {
