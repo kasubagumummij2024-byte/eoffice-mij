76aff5504c5b1e97651a6011ac21852d6909d10a
kasubagumummij2024-byte <kasubagumummij2024@gmail.com>
2025-11-21 07:27:19 +0700

    Fix: app.use header manual

--- app.js
+++ app.js
@@ -33,9 +33,17 @@ if (!admin.apps.length) {
 const db = admin.firestore();
 const app = express();
 
-// --- SETTING CORS (DEFAULT: IZINKAN SEMUA) ---
-app.use(cors()); 
-// ---------------------------------------------
+// --- SETTING CORS MANUAL (HARDCORE) ---
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Izinkan semua domain
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  
+  if (req.method === 'OPTIONS') {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use(express.json());
 
